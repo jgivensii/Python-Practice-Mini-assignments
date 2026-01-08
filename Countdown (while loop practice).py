4450e86143dcf1f14530c3c2610bb397efdb8654
Jelani Givens II <jelanigivensii@gmail.com>
2026-01-08 16:21:18 -0600

    Add files via upload

---
+++ Countdown (while loop practice).py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  1 22:52:28 2026
+
+@author: jgive
+"""
+num = 10
+i=0
+while i in range(num):
+    Countdown=num-i
+    i+=1
+    print(f"{Countdown}")
+      
+if i==10:
+    print("BLASTOFF!")
+
